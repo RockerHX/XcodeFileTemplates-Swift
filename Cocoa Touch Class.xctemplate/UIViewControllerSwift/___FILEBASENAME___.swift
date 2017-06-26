d298ae27a7cb09114d81e4104c15ff08f162a346
RockerHX <RockerHX@gmail.com>
2017-06-26 17:34:13 +0800

    Update[Code]: UIViewController

--- Cocoa Touch Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
+++ Cocoa Touch Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -5,31 +5,33 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //___COPYRIGHT___
 //
+//  GitHub: https://github.com/rockerhx
+//
+
 
 import UIKit
 
+
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
 
+    // MARK: - IBOutlet Property -
+    // MARK: - Public Property -
+    // MARK: - Private Property -
+
+    // MARK: - View Controller Life Cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configure()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Navigation -
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+    // MARK: - Event Methods -
+    // MARK: - Private Methods -
+    fileprivate func configure() {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
